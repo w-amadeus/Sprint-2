d6f8f052f4d3714c41554a1c641cc589a426635f
Jandy Castillo <jandy.castillo@tecsup.edu.pe>
2022-06-04 22:55:36 -0300

    Add: eng names

--- src/index.js
+++ src/index.js
@@ -1,40 +1,39 @@
 const $ = (element) => document.querySelector(element);
 
-const lista = $("#lista-pvtas");
-const nombre = $("#nombre");
-const monto = $("#monto");
+const list = $("#list");
+const personName = $("#name");
+const personAmount = $("#amount");
 const button = $("button");
 const total = $("#total");
-const prom = $("#promedio")
+const toPay = $("#mean");
 
-const amount = [];
+const amounts = [];
 
 const createListItem = (item) => {
   const li = document.createElement("li");
-  li.className="border-solid border-2 m-2 w-60 rounded-sm";
-  li.appendChild(document.createTextNode(`${item.nombre} : $${item.monto}`));
-  lista.appendChild(li);
+  li.className = "border-solid border-2 m-2 w-60 rounded-sm";
+  li.appendChild(document.createTextNode(`${item.name} : $${item.amount}`));
+  list.appendChild(li);
 };
 
-const sum = () => {
-    let total = 0;
-    amount.forEach(element => {
-        total += parseFloat(element.monto); 
-    });
-    
-    return {promedio: total/amount.length, total}
-}
+const getTotalMean = () => {
+  let total = 0;
+  amounts.forEach((element) => {
+    total += parseFloat(element.amount);
+  });
+
+  return { mean: (total / amounts.length).toFixed(2), total };
+};
 
 button.addEventListener("click", (e) => {
-    if (isNaN(monto.value) && nombre.value === undefined) {
-            alert("La recontra perra que te re mil pario");
-            return
-           }
-    const data = { nombre: nombre.value, monto: monto.value };
-  createListItem(data);
-  amount.push(data);
-  const {promedio, total: suma} = sum();
-  total.textContent = `$${suma}`;
-  prom.textContent = `$${promedio}`;
-
+  if (personAmount.value > 0 && personName.value !== "") {
+    const data = { name: personName.value, amount: personAmount.value };
+    createListItem(data);
+    amounts.push(data);
+    const { mean, total: add } = getTotalMean();
+    total.textContent = `$${add}`;
+    toPay.textContent = `$${mean}`;
+  } else {
+    alert("Ingrese datos correctos!");
+  }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
